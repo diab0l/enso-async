c1e8c2efa447513770b29b18f21038f82127fda6
mstramm <stramm@dr-ecklebe>
2022-09-19 00:35:18 +0200

    Add Aff.{map,pure,apply,whenAll,whenAny} to make it generally nicer to work with

--- polyglot/java/bin/experiments/Aff.java
+++ polyglot/java/bin/experiments/Aff.java
@@ -2,6 +2,9 @@ package experiments;
 
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.StreamSupport;
+import java.util.concurrent.CompletableFuture;
+import java.util.Arrays;
 
 public class Aff {
 	private static Aff token = new Aff();
@@ -19,5 +22,44 @@ public class Aff {
 	public static <T> T runAff(App<Aff, T> aff) throws InterruptedException, ExecutionException {
 		CompletionStage<T> x = leave(aff);
 		return x.toCompletableFuture().get();
+	}
+	
+	public static <T> App<Aff, Object[]> whenAll(Iterable<App<Aff, T>> affs) {
+		CompletableFuture<T>[] xs =
+			StreamSupport
+			.stream(affs.spliterator(), false)
+			.map(Aff::leave)
+			.map(CompletionStage::toCompletableFuture)
+			.toArray(s -> new CompletableFuture[s]);
+		
+		CompletableFuture<Void> allDone = CompletableFuture.allOf(xs);
+		CompletableFuture<Object[]> result = 
+			allDone
+			.<Object[]>thenCompose(unused -> {
+				try {
+					Object[] ys = new Object[xs.length];
+					for (int i = 0; i < xs.length; i++) {
+						ys[i] = xs[i].get();
+					}
+					return CompletableFuture.completedFuture(ys);
+				} catch (Throwable ex) {
+					return CompletableFuture.failedFuture(ex);
+				}
+			});
+		
+		return completionStage(result);
+	}
+
+	public static <T> App<Aff, T> whenAny(Iterable<App<Aff, T>> affs) {
+		CompletableFuture<T>[] xs =
+			StreamSupport
+			.stream(affs.spliterator(), false)
+			.map(Aff::leave)
+			.map(CompletionStage::toCompletableFuture)
+			.toArray(s -> new CompletableFuture[s]);
+		
+		CompletableFuture<T> result = (CompletableFuture<T>) CompletableFuture.anyOf(xs);
+		
+		return completionStage(result);
 	}
 }
